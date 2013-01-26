420d4235f508956126871db30885e5c82aa3efaa
Ryan Self <ryan@localhost.(none)>
2013-01-26 15:57:49 -0800

    Move sidebar to the left and allow pressing enter key for form values

--- client/main.js
+++ client/main.js
@@ -14,6 +14,8 @@ $(document).ready(function () {
         if (config.name)  RR.user.setName(config.name);
         if (config.color) RR.user.setColor(config.color);
     });
+    
+    // handle the creation of a new pin, as specified on the socket
     socket.on('new pin', function (config) {
         var pin, upper, lower;
         var pinId = config.pin.id;
@@ -21,19 +23,27 @@ $(document).ready(function () {
         pin = RR.pinMap[pinId] = new RRPin(config.pin);
         upper = pin.getDiagEndpoints().upper;
         lower = pin.getDiagEndpoints().lower;
-        $('#pins').append(Handlebars.templates['pin']({
+        $('#pinList').append(Handlebars.templates['pin']({
             pin:  { id: pinId, lower: lower, upper: upper },
             user: { color: color }
         }));
-        $('#commentInputButton_' + pinId).on('click', (function (id) {
+        var sendComment = (function (id) {
             return function () {
-                RRComment.sendNew({
-                    pin: id,
-                    text: $('#commentInput_' + id).val()
-                });
+                RRComment.sendNew({ pin: id, text: $('#commentInput_' + id).val() });
             };
-        }(pinId)));
+        }(pinId));
+        $('#commentInputButton_' + pinId).on('click', sendComment);
+        var commentInput = $('#commentInput_' + pinId);
+        commentInput.on('keypress', function (eventObj) {
+            if (eventObj.which === 13) {  // enter key
+                sendComment();
+                commentInput.val('');
+                return false;  // prevent form submission
+            }
+        });
     });
+
+    // handle the creation of a new comment, as specified on the socket
     socket.on('new comment', function (config) {
         var comment, commentText;
         var commentId = config.comment.id;
@@ -46,9 +56,20 @@ $(document).ready(function () {
             user: { color: color }
         }));
     });
-    $('#sendName').on('click', function () {
+
+    // handle sending the user name
+    var sendUserName = function () {
         RRUser.sendUpdate({ name: $('#nameToSend').val() });
+    };
+    $('#sendName').on('click', sendUserName);
+    $('#nameToSend').on('keypress', function (eventObj) {
+        if (eventObj.which === 13) { // enter key
+            sendUserName();
+            $('#nameToSend').val('');
+            return false;  // prevent form submission
+        }
     });
+
     $('#sendDiagEndpoints').on('click', function () {
         RRPin.sendNew({
             upper: [ $('#upperXAdd').val(), $('#upperYAdd').val() ],
